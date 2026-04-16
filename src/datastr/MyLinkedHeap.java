5eb8b53e7b3961499fe54764a2f767c945793ee5
ValdisEizans <valdis.eizans@gmail.com>
2026-04-16 10:30:33 +0300

    Edit: MyLinkedHeap add function added

--- src/datastr/MyLinkedHeap.java
+++ src/datastr/MyLinkedHeap.java
@@ -2,7 +2,7 @@ package datastr;
 
 public class MyLinkedHeap<Ttype> {
 	//Mainigie
-	private MyNode<Ttype> root = null;
+	private MyNode<Ttype> rootNode = null;
 	private MyNode<Ttype> lastNode = null;
 	int howManyElements = 0;
 	
@@ -28,6 +28,29 @@ public class MyLinkedHeap<Ttype> {
 			return true;
 		}
 	}
+	
+	//ievietot bloku
+	public void engueue(Ttype element) throws Exception{
+		if(isFull()) {
+			throw new Exception("Kaudze ir pilna, nevar pievienot elementu!");
+		}
+		if(element == null) {
+			throw new Exception("Nav noradits elements!");
+		}
+		//ja pirmais bloks
+		if(isEmpty()) {
+			MyNode<Ttype> newNode = new MyNode<Ttype>(element);
+			rootNode = newNode;
+			lastNode = newNode;
+			howManyElements++;
+		}
+		else {
+			
+		}
+		
+	}
+	
+	
 	
 	
 	
